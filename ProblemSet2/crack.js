083fc51335a06753970a64167b104546a7e86a8f
Cheyanna <cheyanna.graham@gmail.com>
2019-06-20 10:40:16 -0700

    remove unecessary function

--- ProblemSet2/crack.js
+++ ProblemSet2/crack.js
@@ -7,36 +7,14 @@ const crack = (string) => {
     let permutDig = 1;
     let key = [];
     let count = 0;
-    
-    // Initial function to iterate over string chars @ 1 digit
-    const permutate = () => {
-        
-        while (count < string.length) {
+    let strLen = string.length;
 
-            // loop through length of string
-            for (let i = 0; i < string.length; i++) {
-                
-                key[permutDig] = string[i];
-                if (permutDig < 2) {
-                    console.log(key.join(""));
-                }
-            }
-            // At end of string, add digit
-            permutDig++;
-            if (permutDig > string.length) {
-                break;
-            } else {
-                setDigits(permutDig);
-            }
-            count++;
-        }
-    }
-    
+    // Recursive set permutate digits < 1
     const setDigits = digits => {
-        for (let i = 0; i < string.length; i++) {
+        for (let i = 0; i < strLen; i++) {
             // set current value
             key[digits] = string[i];
-            
+
             // change the prev digits
             if (digits > 1) {
                 setDigits(digits - 1);
@@ -46,8 +24,24 @@ const crack = (string) => {
             }
         }
     }
-    
-    permutate();
+    // Iterate over string chars @ 1 digit
+    while (count < strLen) {
+        for (let i = 0; i < strLen; i++) {
+
+            key[permutDig] = string[i];
+            if (permutDig < 2) {
+                console.log(key.join(""));
+            }
+        }
+        // At end of string, add digit
+        permutDig++;
+        if (permutDig > strLen) {
+            break;
+        } else {
+            setDigits(permutDig);
+        }
+        count++;
+    }
 }
 
-crack('CRACK');
+crack('ABC');
